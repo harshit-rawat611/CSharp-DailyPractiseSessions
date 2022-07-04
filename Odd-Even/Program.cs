edd309c15c05cacda1705a47496ae92ddfc86de8
psycho_D <psycho_D@DESKTOP-42NHCC3>
2022-07-04 17:47:10 +0530

    Add project files.

---
+++ Odd-Even/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Odd_Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int userWantsToContinue;
+                do
+                {
+                    Console.Write("\nEnter a number : ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    if (n % 2 == 0)
+                        Console.WriteLine(n + " is an Even number !!!");
+                    else
+                        Console.WriteLine(n + " is an Odd number !!!");
+                    Console.WriteLine("\nPress 1 to continue...");
+                    userWantsToContinue = Convert.ToInt32(Console.ReadLine());
+
+                } while (userWantsToContinue==1);
+                }
+            catch(Exception e )
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
